c546234a66653fe13e5ed701c6cf8ec1b5502ba1
jnh1994 <jnhartshorne@outlook.com>
2020-07-29 21:15:25 +0100

    Add social share and back to top functionality.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
 import './modules/a11y.js';
 import './modules/home-animation.js';
 import './modules/underline-animation.js';
+import './modules/back-to-top.js';
---
+++ src/scripts/modules/back-to-top.js
@@ -0,0 +1,52 @@
+/**
+ * The link that triggers the
+ * back to top behaviour.
+ * @type {HTMLElement}
+ */
+const backToTopButton = document.querySelector('[data-js-hook="back-to-top-button"]');
+
+/**
+ * A list of all focusable elements
+ * on the page.
+ * @type {NodeList}
+ */
+const focusableElements = document.querySelectorAll('button, a, input, select, textarea, [tabindex]:not([tabindex="-1"])');
+
+/**
+ * The options used in the native
+ * scrollTo method.
+ * @type {Object}
+ */
+const scrollOptions = {
+  top: 100,
+  left: 0,
+  behavior: 'smooth'
+};
+
+/**
+ * Whether the device supports
+ * smooth scroll, or not.
+ * @type {Boolean}
+ */
+const supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
+
+/**
+ * Handles moving the user back to the
+ * top of the document.
+ * @param event the event object passed in when
+ * clicking on the button / link.
+ */
+function moveToTop(event) {
+  event.preventDefault();
+
+  // Scroll to top.
+  supportsNativeSmoothScroll ? window.scrollTo(scrollOptions) : window.scrollTo(scrollOptions.left, scrollOptions.top);
+
+  // Focus the first focusable element.
+  console.log(focusableElements[0])
+  focusableElements[0].focus({
+    preventScroll: true,
+  })
+}
+
+backToTopButton.addEventListener('click', moveToTop);
